1ffd008cd783813b0701307a94ec27dec4f6f204
LTChaseJohnson <chasevjohnson@gmail.com>
2014-02-18 19:34:50 -0500

    Adding Lesson 5
    Lesson 5 notebook and program.

---
+++ Lesson 5 Infinite Row of Vortices.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import *
+
+N = 50
+xStart,xEnd = -4.0,4.0
+yStart,yEnd = -2.0,2.0
+x = np.linspace(xStart,xEnd,N)
+y = np.linspace(yStart,yEnd,N)
+X,Y = np.meshgrid(x,y)
+
+def getvelocityvortex(strength,xv,yv,X,Y):
+    u = strength/(2*pi)*(Y-yv)/((X-xv)**2+(Y-yv)**2)
+    v = -strength/(2*pi)*(X-xv)/((X-xv)**2+(Y-yv)**2)
+    return u,v
+
+def getstreamfunctionvortex(strength,xv,yv,X,Y):
+    psi = strength/(4*pi)*np.log((X-xv)**2+(Y-yv)**2)
+    return psi
+
+gamma = 5.0
+Nv = 10
+xVortex = np.linspace(xStart,xEnd,Nv)
+yVortex = np.zeros(Nv)
+u = np.zeros((N,N))
+v = np.zeros((N,N))
+
+for i in range(0,Nv):
+    uVortex,vVortex = getvelocityvortex(gamma,xVortex[i],yVortex[i],X,Y)
+    psiVortex = getstreamfunctionvortex(gamma,xVortex[i],yVortex[i],X,Y)
+    u = u + uVortex
+    v = v + vVortex
+
+size = 10
+plt.figure(figsize=(size,(yEnd-yStart)/(xEnd-xStart)*size))
+plt.xlabel('x',fontsize=16)
+plt.ylabel('y',fontsize=16)
+plt.title('Finite Vortices')
+plt.xlim(xStart,xEnd)
+plt.ylim(yStart,yEnd)
+plt.streamplot(X,Y,u,v,density=2.0,linewidth=1,arrowsize=1,arrowstyle='->')
+plt.scatter(xVortex,yVortex,c='#CD2305',s=80,marker='o')
+
+a = 1
+xinfVortex = np.arange(xStart,xEnd,a)
+yinfVortex = np.zeros_like(xinfVortex)
+
+def getvelocityinfvortex(strength,xv,yv,X,Y):
+    u = strength/(2*a)*np.sinh(2*pi*Y/a)/(np.cosh(2*pi*Y/a)-np.cos(2*pi*X/a))
+    v = -strength/(2*a)*np.sin(2*pi*X/a)/(np.cosh(2*pi*Y/a)-np.cos(2*pi*X/a))
+    return u,v
+
+uinfVortex,vinfVortex = getvelocityinfvortex(gamma,xinfVortex,yinfVortex,X,Y)
+
+plt.figure(figsize=(size,(yEnd-yStart)/(xEnd-xStart)*size))
+plt.xlabel('x',fontsize=16)
+plt.ylabel('y',fontsize=16)
+plt.title('Infinite Vortices')
+plt.xlim(xStart,xEnd)
+plt.ylim(yStart,yEnd)
+plt.streamplot(X,Y,uinfVortex,vinfVortex,density=2.0,linewidth=1,arrowsize=1,arrowstyle='->')
+plt.scatter(xinfVortex,yinfVortex,c='#CD2305',s=80,marker='o')
+
+plt.show()
