a07c5780b31115b7a9c754c5591e0b2ca84713f9
LTChaseJohnson <chasevjohnson@gmail.com>
2014-02-19 15:13:33 -0500

    Modified Lesson 5
    User input for vortex strength and numbers.

--- Lesson 5 Infinite Row of Vortices.py
+++ Lesson 5 Infinite Row of Vortices.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import *
 
-N = 50
-xStart,xEnd = -4.0,4.0
+N = 200
+xStart,xEnd = -6.0,6.0
 yStart,yEnd = -2.0,2.0
 x = np.linspace(xStart,xEnd,N)
 y = np.linspace(yStart,yEnd,N)
@@ -18,8 +18,8 @@ def getstreamfunctionvortex(strength,xv,yv,X,Y):
     psi = strength/(4*pi)*np.log((X-xv)**2+(Y-yv)**2)
     return psi
 
-gamma = 5.0
-Nv = 10
+gamma = input('Enter vortex strength: ')
+Nv = input('Enter number of vortices: ')
 xVortex = np.linspace(xStart,xEnd,Nv)
 yVortex = np.zeros(Nv)
 u = np.zeros((N,N))
@@ -41,7 +41,7 @@ plt.ylim(yStart,yEnd)
 plt.streamplot(X,Y,u,v,density=2.0,linewidth=1,arrowsize=1,arrowstyle='->')
 plt.scatter(xVortex,yVortex,c='#CD2305',s=80,marker='o')
 
-a = 1
+a = input('Enter spacing for infinite vortex sheet: ')
 xinfVortex = np.arange(xStart,xEnd,a)
 yinfVortex = np.zeros_like(xinfVortex)
 
@@ -60,29 +60,5 @@ plt.xlim(xStart,xEnd)
 plt.ylim(yStart,yEnd)
 plt.streamplot(X,Y,uinfVortex,vinfVortex,density=2.0,linewidth=1,arrowsize=1,arrowstyle='->')
 plt.scatter(xinfVortex,yinfVortex,c='#CD2305',s=80,marker='o')
-
-gamma = 5.0
-Nv = 100
-xVortex1 = np.linspace(-50,50,Nv)
-yVortex1 = np.zeros(Nv)
-u1 = np.zeros((N,N))
-v1 = np.zeros((N,N))
-
-for i in range(0,Nv):
-    uVortex1,vVortex1 = getvelocityvortex(gamma,xVortex1[i],yVortex1[i],X,Y)
-    psiVortex1 = getstreamfunctionvortex(gamma,xVortex1[i],yVortex1[i],X,Y)
-    u1 = u1 + uVortex1
-    v1 = v1 + vVortex1
-
-size = 10
-plt.figure(figsize=(size,(yEnd-yStart)/(xEnd-xStart)*size))
-plt.xlabel('x',fontsize=16)
-plt.ylabel('y',fontsize=16)
-plt.title('Finite Vortices')
-plt.xlim(xStart,xEnd)
-plt.ylim(yStart,yEnd)
-plt.streamplot(X,Y,u1,v1,density=2.0,linewidth=1,arrowsize=1,arrowstyle='->')
-plt.scatter(xVortex1,yVortex1,c='#CD2305',s=80,marker='o')
-
 
 plt.show()
